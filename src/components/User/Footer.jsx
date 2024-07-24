ef9ef087f8b962fed5d94c20cd23ca069061866f
unknown <darweshglobal.marketing@gmail.com>
2024-07-24 18:25:14 +0530

    email updated

--- src/components/User/Footer.jsx
+++ src/components/User/Footer.jsx
@@ -7,14 +7,14 @@ function Footer({ showForm }) {
   return (
     <div>
       <hr />
-      <footer class="relative z-10 lg:p-5 bg-white pb-10 pt-5 lg:pt-[50px]">
-        <div class="container mx-auto">
-          <div class="flex flex-wrap lg:gap-16">
-            <div class="w-full px-4 sm:w-2/3 lg:w-3/12">
-              <div class="w-full mb-10 lg:ml-10">
+      <footer className="relative z-10 lg:p-5 bg-white pb-10 pt-5 lg:pt-[50px]">
+        <div className="container mx-auto">
+          <div className="flex flex-wrap lg:gap-16">
+            <div className="w-full px-4 sm:w-2/3 lg:w-3/12">
+              <div className="w-full mb-10 lg:ml-10">
                 <a
                   href="#"
-                  class="mb-6 text-green-600 text-xl font-bold inline-block"
+                  className="mb-6 text-green-600 text-xl font-bold inline-block"
                 >
                   DARWESH GROUP
                 </a>
@@ -24,7 +24,7 @@ function Footer({ showForm }) {
 
                     <a className="font-bold"> RAICHUR </a>
                   </div>
-                  <p class="text-base text-body-color mb-7">
+                  <p className="text-base text-body-color mb-7">
                     Near Toyota Showroom, OPP : RIMS Gate, Hydarabad Road,
                     Raichur. 584102.
                   </p>
@@ -32,9 +32,9 @@ function Footer({ showForm }) {
               </div>
             </div>
 
-            <div class="w-full sm:w-1/2 lg:w-2/12">
-              <div class="w-full mb-10 lg:p-0 p-5">
-                <h4 class="text-lg font-bold mb-4">Get in touch </h4>
+            <div className="w-full sm:w-1/2 lg:w-2/12">
+              <div className="w-full mb-10 lg:p-0 p-5">
+                <h4 className="text-lg font-bold mb-4">Get in touch </h4>
                 <ul>
                   <li className="flex items-center gap-1 mt-5">
                     <MdEmail />
@@ -57,14 +57,14 @@ function Footer({ showForm }) {
                 </ul>
               </div>
             </div>
-            <div class="w-full px-4 sm:w-1/2 lg:w-2/12">
-              <div class="w-full mb-10">
-                <h4 class="text-lg font-bold text-dark mb-4">Quick Links</h4>
-                <ul class="space-y-3">
+            <div className="w-full px-4 sm:w-1/2 lg:w-2/12">
+              <div className="w-full mb-10">
+                <h4 className="text-lg font-bold text-dark mb-4">Quick Links</h4>
+                <ul className="space-y-3">
                   <li>
                     <a
                       href="/claims"
-                      class="inline-block text-base leading-loose text-body-color hover:text-primary "
+                      className="inline-block text-base leading-loose text-body-color hover:text-primary "
                     >
                       Claims{" "}
                     </a>
@@ -72,7 +72,7 @@ function Footer({ showForm }) {
                   <li>
                     <a
                       href="/track"
-                      class="inline-block text-base leading-loose text-body-color hover:text-primary "
+                      className="inline-block text-base leading-loose text-body-color hover:text-primary "
                     >
                       Track claims
                     </a>
@@ -80,7 +80,7 @@ function Footer({ showForm }) {
                   {/* <li>
                     <a
                       href="/contact"
-                      class="inline-block text-base leading-loose text-body-color hover:text-primary "
+                      className="inline-block text-base leading-loose text-body-color hover:text-primary "
                     >
                       Contact Us{" "}
                     </a>
@@ -88,14 +88,14 @@ function Footer({ showForm }) {
                 </ul>
               </div>
             </div>
-            <div class="w-full px-4 sm:w-1/2 lg:w-2/12">
-              <div class="w-full mb-10">
-                <h4 class="text-lg font-bold text-dark mb-4">Company </h4>
-                <ul class="space-y-3">
+            <div className="w-full px-4 sm:w-1/2 lg:w-2/12">
+              <div className="w-full mb-10">
+                <h4 className="text-lg font-bold text-dark mb-4">Company </h4>
+                <ul className="space-y-3">
                   <li>
                     <a
                       href="/privacy-policy"
-                      class="inline-block text-base leading-loose text-body-color hover:text-primary "
+                      className="inline-block text-base leading-loose text-body-color hover:text-primary "
                     >
                       Privacy Policy{" "}
                     </a>
@@ -103,7 +103,7 @@ function Footer({ showForm }) {
                   <li>
                     <a
                       href="/terms"
-                      class="inline-block text-base leading-loose text-body-color hover:text-primary "
+                      className="inline-block text-base leading-loose text-body-color hover:text-primary "
                     >
                       Terms & Conditions{" "}
                     </a>
@@ -111,7 +111,7 @@ function Footer({ showForm }) {
                   {/* <li>
                     <a
                       href="#"
-                      class="inline-block text-base leading-loose text-body-color hover:text-primary "
+                      className="inline-block text-base leading-loose text-body-color hover:text-primary "
                     >
                       How To Apply ?
                     </a>
